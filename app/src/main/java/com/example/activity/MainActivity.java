f193c45b426ebc4d84656514a233678067403389
pyz6666 <893758346@qq.com>
2021-02-27 22:11:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.activity;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mButton ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initOnClick();
+    }
+
+    private void initOnClick() {
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.
+                       permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                   ActivityCompat.requestPermissions(MainActivity.this,new
+                           String[]{Manifest.permission.CALL_PHONE},1
+                           );
+                   /*判断是否授权*/
+               } else {
+                   call();
+               }
+            }
+        });
+    }
+    private void call(){
+        Intent intent =new Intent();
+        intent.setAction(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:10086"));
+        startActivity(intent);
+    }
+
+    private void initView() {
+        mButton =findViewById(R.id.make_call);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    call();
+                } else {
+                    Toast.makeText(this,"you denied is premission",Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+        }
+    }
+}
---
+++ app/src/main/java/com/example/activity/NewAdapter.java
@@ -0,0 +1,21 @@
+package com.example.activity;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class NewAdapter extends ArrayAdapter {
+    public NewAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+}
